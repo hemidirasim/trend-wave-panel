91998bd8cee0a1e4e436aa083f4cc50b43ccd5eb
hemidirasim <hamidirasim@gmail.com>
2025-07-26 11:49:23 +0400

    asda

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -217,7 +217,7 @@ const Order = () => {
       newErrors.serviceId = t('order.serviceRequired');
     }
     if (!formData.url.trim()) {
-      newErrors.url = '{t('order.requiredUrl')}';
+      newErrors.url = t('order.requiredUrl');
     } else if (!proxyApiService.validateUrl(selectedPlatform, formData.url)) {
       newErrors.url = 'Düzgün URL formatı daxil edin';
     }
